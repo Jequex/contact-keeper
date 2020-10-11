6a9320d65a8f5da164d36eab181c73d7bebfe690
jequex <onyejekwe.harddisk20@gmail.com>
2020-10-11 11:34:11 +0100

    backend done

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+
+    try {
+        await mongoose.connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+
+    console.log("mongoDB connected...")
+    } catch (err) {
+        console.error(err.message);
+        console.log("unable to connect, check your internet connection")
+        process.exit(1);
+    }
+    
+}
+
+module.exports = connectDB;
